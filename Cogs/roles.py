23c1c6798b5b4d0066cdd1e7f9d343e2e54dc81c
NekoMimi <nekomimiofficial@proton.me>
2025-09-03 17:59:02 +0300

    added verification

--- Cogs/roles.py
+++ Cogs/roles.py
@@ -6,7 +6,7 @@ from NekoMimi import reg as nreg
 
 class RolesCog(commands.Cog):
     def __init__(self, bot: commands.Bot):
-        bot= bot;
+        self.bot= bot;
 
     role_commands= app_commands.Group(name= "roles", description= "Manage multiple role settings")
 
@@ -59,6 +59,51 @@ class RolesCog(commands.Cog):
         db= nreg.Database(regName)
         db.store("join-role", "0:")
         await interaction.response.send_message(f"Join role disabled.")
+
+    @role_commands.command(name= "user-verify", description= "A tool to grant users a role when they manage to verify successfully.")
+    @app_commands.guild_only()
+    @app_commands.checks.has_permissions(administrator= True)
+    @app_commands.describe(verifygrantrole= "The role to grant members who succeed in the verification.")
+    async def __CMD_verity_cs(self, interaction: discord.Interaction, verifygrantrole: discord.Role):
+        db= nreg.Database(f"Celestia-Guilds-{interaction.guild_id}")
+        db.store("verity-cs", str(verifygrantrole.id))
+        await interaction.response.send_message("❀  Done, you may now rest peacefully knowing no bots will join :3", ephemeral= True)
+        await interaction.channel.send(f"✿  Welcome to **{interaction.guild.name}**!\nPlease verify yourself to get access to the {verifygrantrole.mention} role.", allowed_mentions= discord.AllowedMentions.none(), view= self.Verifier())
+
+
+    class VerificationButton(discord.ui.Button):
+        def __init__(self):
+            super().__init__(label= "Verify", style= discord.ButtonStyle.green, custom_id= "CelestiaVerifyService")
+
+        async def callback(self, interaction: discord.Interaction):
+            db= nreg.Database(f"Celestia-Guilds-{interaction.guild_id}")
+            print("start operation")
+            roleID= int(db.query("verity-cs"))
+            print("get RID")
+            role= interaction.guild.get_role(roleID)
+            print("get Role")
+            if role:
+                try:
+                    await interaction.user.add_roles(role, reason= "Verified successfully")
+                    print("assign Role")
+                except Exception as e:
+                    await interaction.response.send_message(f"Internal error: {e}\n\nPlease report this to the devs")
+                await interaction.response.send_message("❀  Access granted!\nHave a safe stay :pink_heart:", ephemeral= True)
+            else:
+                await interaction.response.send_message("❀  Error granting role!\nPlease report this to the admins", ephemeral= True)
+
+    class Verifier(discord.ui.View):
+        def __init__(self):
+            super().__init__(timeout=None)
+            self.add_item(RolesCog.VerificationButton())
+
+    @commands.Cog.listener()
+    async def on_ready(self):
+        try:
+            self.bot.add_view(self.Verifier())
+            print("[  ok  ] attaching Verifier view")
+        except Exception as e:
+            print(f"[ fail ] attaching Verifier: {e}")
 
     @commands.Cog.listener()
     async def on_member_join(self, member: discord.Member):
