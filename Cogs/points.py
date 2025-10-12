849dd4034efd754f3ba00f47cd4f0818ff2d8c99
NekoMimi <nekomimiofficial@proton.me>
2025-10-13 01:40:48 +0300

    patched to next level again...

--- Cogs/points.py
+++ Cogs/points.py
@@ -23,7 +23,8 @@ def anti_lvl(p: int):
     return int(math.ceil((DIFF_FACTOR ** p) * DIFFICULTY))
 
 def to_next_lvl(p:int):
-    return int(anti_lvl(lvl(p)) - p)
+    x= (p - lvl(p)) if p > lvl(p) else p
+    return int(anti_lvl(lvl(p)) - x)
 
 async def get_point_count(uid, gid):
     await sqldb.connect()
