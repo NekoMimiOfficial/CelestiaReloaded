4838acd77d50b1ea6b1f4a3071ad36ff00e651f1
NekoMimi <nekomimiofficial@proton.me>
2025-10-13 14:03:23 +0300

    handle excptions in new async httpx rewrite

--- Cogs/necho/cables.py
+++ Cogs/necho/cables.py
@@ -1,5 +1,6 @@
 import httpx
 import asyncio
+from httpx import HTTPStatusError, RequestError, TimeoutException
 
 def time_chatting(p: int):
     sec = p
@@ -13,27 +14,41 @@ def time_chatting(p: int):
     return f"{hours}:{mins % 60}:{sec % 60}"
 
 async def get_track(term, api):
-    async with httpx.AsyncClient() as client:
-        req = await client.get(url=api + "/search/", params={'s': term})
-        jsonObj = req.json()
-        if not "items" in jsonObj:
-            return False
-        items = jsonObj["items"]
-        if len(items) < 1:
-            return False
-        songID = items[0]["id"]
-        getUrlPlayable = await client.get(url=api + "/track/", params={'id': songID, 'quality': 'LOW'})
-        gson = getUrlPlayable.json()
-        if not len(gson) > 0:
-            return False
-        if getUrlPlayable.text.replace("\"", "'").startswith("{'detail"):
-            return False
-        return {
-            'url': gson[-1]["OriginalTrackUrl"],
-            'cover': "https://resources.tidal.com/images/" + items[0]["album"]["cover"].replace("-", "/") + "/640x640.jpg",
-            'id': songID,
-            'title': items[0]['title'],
-            'duration': time_chatting(int(items[0]['duration'])),
-            'artist': items[0]["artist"]["name"],
-            'album': items[0]["album"]["title"]
-            }
+    try:
+        async with httpx.AsyncClient() as client:
+            try:
+                req = await client.get(url=api + "/search/", params={'s': term})
+                req.raise_for_status()
+            except (HTTPStatusError, RequestError, TimeoutException):
+                return False
+
+            jsonObj = req.json()
+            if not "items" in jsonObj:
+                return False
+            items = jsonObj["items"]
+            if len(items) < 1:
+                return False
+            songID = items[0]["id"]
+
+            try:
+                getUrlPlayable = await client.get(url=api + "/track/", params={'id': songID, 'quality': 'LOW'})
+                getUrlPlayable.raise_for_status()
+            except (HTTPStatusError, RequestError, TimeoutException):
+                return False
+
+            gson = getUrlPlayable.json()
+            if not len(gson) > 0:
+                return False
+            if getUrlPlayable.text.replace("\"", "'").startswith("{'detail"):
+                return False
+            return {
+                'url': gson[-1]["OriginalTrackUrl"],
+                'cover': "https://resources.tidal.com/images/" + items[0]["album"]["cover"].replace("-", "/") + "/640x640.jpg",
+                'id': songID,
+                'title': items[0]['title'],
+                'duration': time_chatting(int(items[0]['duration'])),
+                'artist': items[0]["artist"]["name"],
+                'album': items[0]["album"]["title"]
+                }
+    except Exception:
+        return False
