b8c3085d3e9aaa68ea0ed1459aab8be25b7d31c7
NekoMimi <nekomimiofficial@proton.me>
2026-05-23 15:05:47 +0300

    el goog tts

--- Cogs/tts.py
+++ Cogs/tts.py
@@ -2,22 +2,49 @@ import discord
 from discord import app_commands
 from discord.ext import commands
 
-import pyttsx3
-import asyncio
+
+from google import genai
+from google.genai import types
+import wave
 import os
+
+from NekoMimi import reg
+
+def wave_file(filename, pcm, channels=1, rate=24000, sample_width=2):
+   with wave.open(filename, "wb") as wf:
+      wf.setnchannels(channels)
+      wf.setsampwidth(sample_width)
+      wf.setframerate(rate)
+      wf.writeframes(pcm)
 
 class TTSCog(commands.Cog):
     def __init__(self, bot: commands.Bot) -> None:
         self.bot= bot
-        self.engine= pyttsx3.init()
+        self.client = genai.Client(api_key= reg.readCell("elGoog"))
 ##############################################
 
     @app_commands.command(name= "tts", description= "play a tts into a vc from a message")
     @app_commands.describe(text= "the text to be TTS-ed")
     async def com_tts(self, interaction: discord.Interaction, text:str):
         file= str(interaction.user.id)+ "-tts.mp3"
-        self.engine.save_to_file(text, file)
-        await asyncio.to_thread(self.engine.runAndWait)
+        response = client.models.generate_content(
+           model="gemini-3.1-flash-tts-preview",
+           contents=f"Say cheerfully: {text}",
+           config=types.GenerateContentConfig(
+              response_modalities=["AUDIO"],
+              speech_config=types.SpeechConfig(
+                 voice_config=types.VoiceConfig(
+                    prebuilt_voice_config=types.PrebuiltVoiceConfig(
+                       voice_name='Kore',
+                    )
+                 )
+              ),
+           )
+        )
+
+        data = response.candidates[0].content.parts[0].inline_data.data
+
+        wave_file(file, data)
 
         await interaction.response.defer()
         if not interaction.user.voice or not interaction.user.voice.channel:
